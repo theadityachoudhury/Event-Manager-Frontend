6fa7a76ca1e6d2620e60dc2599973c93268c8a8f
Aditya Choudhury <adityasubham03@gmail.com>
2023-12-23 15:28:46 +0530

    Added all the auth pages

--- Frontend/Schemas/index.ts
+++ Frontend/Schemas/index.ts
@@ -1,4 +1,4 @@
-import { z, ZodError } from "zod";
+import { z } from "zod";
 
 export const Loginschema = z.object({
     email: z.string().email("Invalid email format").min(1),
@@ -8,6 +8,21 @@ export const Loginschema = z.object({
 export const Signupschema = z.object({
     email: z.string().email("Invalid email format").min(1),
     name: z.string().min(1, "Name is mandatory"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    cpassword: z.string().min(6, "Password must be at least 6 characters"),
+});
+
+
+export const Verifyschema = z.object({
+    email: z.string().email("Invalid email format").min(1),
+    otp: z.string().min(6, "OTP must be at least 6 characters"),
+});
+
+export const Forgetschema = z.object({
+    email: z.string().email("Invalid email format").min(1),
+});
+
+export const Resetschema = z.object({
     password: z.string().min(6, "Password must be at least 6 characters"),
     cpassword: z.string().min(6, "Password must be at least 6 characters"),
 });
--- Frontend/types/index.ts
+++ Frontend/types/index.ts
@@ -1,6 +1,12 @@
-import { Loginschema, Signupschema } from "@/Schemas";
+import { Forgetschema, Loginschema, Resetschema, Signupschema, Verifyschema } from "@/Schemas";
 import { z } from "zod";
 
 export type loginFormData = z.infer<typeof Loginschema>;
 export type signupFormData = z.infer<typeof Signupschema>;
+export type VerifyFormData = z.infer<typeof Verifyschema>;
+export type ForgetFormData = z.infer<typeof Forgetschema>;
+export type ResetFormData = z.infer<typeof Resetschema>;
+
+
+
 
