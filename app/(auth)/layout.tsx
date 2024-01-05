92e0d6ed124d10913bd0fbf7c3c166bda05b0083
Aditya Choudhury <adityasubham03@gmail.com>
2024-01-05 14:19:06 +0530

    Removed nextJS

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <div className="w-full min-h-screen justify-evenly">
-        {children}
-      </div>
-    )
-  }
-  
-  export default Layout
