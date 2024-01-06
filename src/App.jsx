593f72329d321b0d36b358917eed5fb55d9b6b39
Aditya Choudhury <adityasubham03@gmail.com>
2024-01-06 23:18:13 +0530

    Added login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,32 @@ import "./App.css";
 import Home from "./pages/Home";
 import { UserContextProvider } from "./UserContext";
 import Login from "./pages/Login";
+import Verify from "./pages/Verify";
+import Dashboard from "./pages/Dashboard";
+import Layout from "./Layout";
+import axios from "axios";
+const baseURL =
+	window.location.hostname === "evently.adityachoudhury.com"
+		? "https://backend.evently.adityachoudhury.com"
+		: "http://localhost:5000";
+
+axios.defaults.baseURL = baseURL;
+axios.defaults.withCredentials = true;
 
 function App() {
-  return (
-    <UserContextProvider>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route index element={<Home />} />
-      </Routes>
-    </UserContextProvider>
-  );
+	return (
+		<UserContextProvider>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route path="/dashboard" element={<Dashboard />} />
+				</Route>
+				<Route path="/login" element={<Login />} />
+				<Route path="/verify" element={<Verify />} />
+
+				<Route index element={<Home />} />
+			</Routes>
+		</UserContextProvider>
+	);
 }
 
 export default App;
