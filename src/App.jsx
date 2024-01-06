b46302ad27f53a60e15a3ec3288354ed51895586
Aditya Choudhury <adityasubham03@gmail.com>
2024-01-06 21:52:25 +0530

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,35 @@
-import axios from "axios";
-import "./App.css";
-import { Navigate, Route, Routes } from "react-router-dom";
-import { UserContextProvider } from "./UserContext";
-import Home from "./Components/Home";
-
-const baseURL =
-	window.location.hostname === "evently.adityachoudhury.com"
-		? "https://backend.evently.adityachoudhury.com"
-		: "http://localhost:5000";
-
-axios.defaults.baseURL = baseURL;
-axios.defaults.withCredentials = true;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-	return (
-		<UserContextProvider>
-			<Routes>
-				<Route path="/" index element={<Home />} />
-			</Routes>
-		</UserContextProvider>
-	);
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
