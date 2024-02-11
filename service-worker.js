92bf16dfbf8910528aa4f879e72a08c5450f3ef1
Aditya Choudhury <adityasubham03@gmail.com>
2024-02-11 22:49:00 +0530

    Added install for mobile devices

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,13 @@
 importScripts(
     'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
 );
-  
+
 workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'js',
     new workbox.strategies.CacheFirst()
 );
