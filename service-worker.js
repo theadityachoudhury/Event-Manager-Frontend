38c153f6facdec0a0b8ce87d5869b2752d3f977f
Aditya Choudhury <adityasubham03@gmail.com>
2024-02-11 23:34:52 +0530

    PWA related useful updates

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,31 @@ workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
 );
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'script',
+    new workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'style',
+    new workbox.strategies.CacheFirst()
+);
+
+
+let promptShown = false;
+
+self.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent the browser's default install prompt
+    event.preventDefault();
+
+    // Show the install prompt only if it hasn't been dismissed before
+    if (!promptShown) {
+        event.prompt();
+    }
+});
+
+// Set a flag to indicate that the install prompt has been shown
+self.addEventListener('appinstalled', () => {
+    promptShown = true;
+});
